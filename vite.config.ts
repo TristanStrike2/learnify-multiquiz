b873d1bdff1db2c7639efdd4ab8ab853f2792fe6
TristanStrike <tristan@ignitecpp.co.za>
2025-03-13 16:34:52 +0200

    Fix deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,13 @@ export default defineConfig({
   build: {
     outDir: "dist",
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
   },
+  base: "/",
 });
